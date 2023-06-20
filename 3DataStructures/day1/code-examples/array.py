e3ec1217d76aa51d54cf0f6b9583220f19adc5d0
Benjamin Cohen <bac1087@gmail.com>
2023-06-20 11:45:40 -0600

    added week03/day1 notes

--- 3DataStructures/day1/code-examples/array.py
+++ 3DataStructures/day1/code-examples/array.py
@@ -1,18 +1,39 @@
+# O(1)
+def lookup_by_index(lst, index):
+    return lst[index]
+
+
 # find_by_index takes a list and a target value
 # and returns the first index matching that value
 # if it exists. Otherwise it returns -1.
+# O(N)
 def find_by_index(lst, target):
+    counter = 0
     for index in range(len(lst)):
+        counter += 1
         if lst[index] == target:
+            print(counter)
             return index
     else:
-      return -1
+        print(counter)
+        return -1
+
+
+def find_by_index_naive(lst, target):
+    counter = 0
+    answer = -1
+    for index in range(len(lst)):
+        counter += 1
+        if lst[index] == target:
+            answer = index
+
+    print(counter)
+    return answer
+
 
 # test it out
-lst = [1, 4, 7, 99, 2, 42]
-print(find_by_index(lst, 1)) # 0
-print(find_by_index(lst, 42)) # 5
-print(find_by_index(lst, 100)) # -1
-
-
-
+lst = list(range(1000))
+find_by_index(lst, 999)
+find_by_index_naive(lst, 999)
+# print(find_by_index(lst, 142))
+# print(find_by_index(lst, 999))
