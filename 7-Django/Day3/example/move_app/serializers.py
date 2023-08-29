5dedbd9538dc832b5d01cf7e35f340fad325b7a5
fravila08 <fr4v1l4@gmail.com>
2023-08-29 10:05:54 -0700

    7-Django/3-associations-and-django-server comp

--- 7-Django/Day3/example/move_app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Move
-
-class MoveSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Move
-        fields = ['id', 'power', 'accuracy']
