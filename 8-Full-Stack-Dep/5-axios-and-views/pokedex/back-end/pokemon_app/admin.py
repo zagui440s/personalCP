b5e344d568f368da5dca58bc003abee01a3fc023
fravila08 <fr4v1l4@gmail.com>
2024-03-29 15:25:39 -0700

    full stack auth

--- 8-Full-Stack-Dep/5-axios-and-views/pokedex/back-end/pokemon_app/admin.py
+++
@@ -1,7 +0,0 @@
-# Pokemon_app/admin.py
-from django.contrib import admin
-# Explecit import of Pokemon Model
-from .models import Pokemon
-
-# Register your models here.
-admin.site.register([Pokemon])
