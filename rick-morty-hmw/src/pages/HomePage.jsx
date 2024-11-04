f9fbb5fcec9bf3f85105cb124c46fdf88afbcb0b
Jesus Aguilar <zaguilar2142@gmail.com>
2024-11-04 10:28:32 -0600

    rick-morty-update1

---
+++ rick-morty-hmw/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+function HomePage() {
+    return (
+      <div>
+        <h2>Get ready for some interdimensional adventures!</h2>
+        <p>Don’t miss out on the wild world of Rick and Morty!</p>
+      </div>
+    );
+  }
+  
+  export default HomePage;
+  
